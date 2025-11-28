3963a628636d7250d26b62655c7fadeebfd0454a
SnoopySong <joh.anne.theo.tom@gmail.com>
2025-11-28 21:46:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import express from "express";
+import { registerRoutes } from "../server/routes";
+import { serveStatic } from "../server/static";
+import { createServer } from "http";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-// Import the built server
-const handler = require('../dist/index.cjs');
+const app = express();
+const httpServer = createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let routesRegistered = false;
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  if (!routesRegistered) {
+    await registerRoutes(httpServer, app);
+    await serveStatic(app);
+    routesRegistered = true;
+  }
+
+  app(req, res);
+};
 
 export default handler;
